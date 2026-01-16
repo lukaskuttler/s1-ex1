8262502a2648e248fa26633285685f927d20f601
lukaskuttler <luka.kuttler2@gmail.com>
2026-01-16 15:10:03 +0100

    This is my first homework and my first time using python.

---
+++ Home Work 1.py
@@ -0,0 +1,12 @@
+#Print allows you to display words and objects
+print("Hi! This is my first Home work on Python!")
+print("Python is not as easy at it looks!")
+#Practice with Numbers
+numbers=[1,2,3,4,5,6,8,7,8,9,10]
+print(numbers)
+#You can also sum them up!
+sum=sum(numbers)
+print(sum)
+#And, you can make a set, which removes duplicates and then counts how many items are in that set.
+LukasList = len(set(numbers))
+print(LukasList)
